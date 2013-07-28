7dc21978a7bc996229c2eb8a620b28068f3f2f7e
ruby -v <architajain.princy@gmail.com>
2013-07-28 19:58:38 +0530

    aaaaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Archi::Application.routes.draw do
 
-  root: 'index#index'
+  root to: 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

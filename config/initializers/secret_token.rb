62a545defbc781c9230730c9a38cc3ca38b64005
ruby -v <architajain.princy@gmail.com>
2013-07-28 19:25:52 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Archi::Application.config.secret_key_base = '16d8652ebbfee10f07a1ce8f464556ef1603c8d696fa930b72d8f0675cafb8d3d29d74a668ab890d7d084d8cb6a4c6bfddf604e2304ca48a55a6f9dd15c1c1f7'
